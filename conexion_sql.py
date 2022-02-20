9a9ecd4a21f5541a27cf36213edb89e0ff7b0a34
cytesting <lowenhard@gmail.com>
2022-02-20 08:13:23 +0000

    Finalizar el script principal

--- conexion_sql.py
+++ conexion_sql.py
@@ -1,11 +1,10 @@
 """
-Module to connect to database
+Módulo para conectarse con la base de datos
 """
 import logging
 import subprocess
 
 from decouple import config
-
 import sqlalchemy
 
 from procesos_pandas import datos_pandas
@@ -15,24 +14,24 @@ engine = sqlalchemy.create_engine(config('DATABASE_URL'))
 DATABASE_NAME = config('DATABASE_NAME')
 
 def crear_tablas():
-    """ Crear las tablas """
+    """ Crear las tablas sql """
     subprocess.run(['psql', '-d', DATABASE_NAME, '-f', 'db_script.sql'], check=True)
+    logging.info('Se crearon las tablas en la base de datos...')
 
 def insertar_datos():
-    """ Insertar datos """
+    """ Insertar datos a la base de datos """
     datos = datos_pandas()
     try:
         datos[0].to_sql('tabla_unificada', con=engine, if_exists='replace', index=False)
         datos[1].to_sql('registros_categoria', con=engine, if_exists='replace', index=False)
         datos[2].to_sql('registros_cine', con=engine, if_exists='replace', index=False)
-        logging.info('All data saved to db...')
+        logging.info('Se guardó todo en la base de datos...')
     except sqlalchemy.exc.OperationalError:
-        logging.error('Could not connect to database')
+        logging.error('No se pudo conectar a la base de datos')
 
-def revisar_db():
-    """ Comprobar y testear """
-    print(engine.execute("SELECT * FROM tabla_unificada").fetchone())
-    print(engine.execute("SELECT * FROM registros_categoria").fetchone())
-    print(engine.execute("SELECT * FROM registros_cine").fetchone())
+def main():
+    crear_tablas()
+    insertar_datos()
 
-insertar_datos()
+if __name__ == '__main__':
+    main()
